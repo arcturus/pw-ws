9bfce109a2b7bff7ad125e0fafc537d380e72d75
Sam Giles <sam.e.giles@gmail.com>
2016-05-31 16:00:28 +0100

    Implement maximum stale value and move values to config

--- test/utils/cache.js
+++ test/utils/cache.js
@@ -19,6 +19,35 @@ describe('ServeStaleCache', () => {
     }).catch(done);
   });
 
+  it('should not serve stale content after two hours', (done) => {
+    const cache = new Cache();
+    const twoHours = 60 * 60 * 2 * 1000;
+
+    let nextTime = 0;
+    cache._now = function() {
+      return nextTime;
+    };
+
+    cache.get('id_q', () => {
+      return {
+        ttl: twoHours,
+        value: 'FirstCachedValue'
+      };
+    }).then((value) => {
+      assert.equal(value, 'FirstCachedValue');
+      nextTime += (twoHours * 2) + 1;
+      return cache.get('id_q', () => {
+        return {
+          value: 'NewCachedValue'
+        };
+      });
+    }).then((value) => {
+      assert.equal(value, 'NewCachedValue');
+      done();
+    }).catch(done);
+
+  });
+
   it('should serve stale content, and rebuild in background', (done) => {
     const cache = new Cache();
 
@@ -32,11 +61,11 @@ describe('ServeStaleCache', () => {
       // Create initial value in cache.
       return {
         ttl: 100,
-        value: 'CachedValue'
+        value: '1'
       };
     }).then((value) => {
       // Assert initial value
-      assert.equal(value, 'CachedValue');
+      assert.equal(value, '1');
 
       // Step forward in time
       nextTime = 101;
@@ -47,25 +76,39 @@ describe('ServeStaleCache', () => {
       return cache.get('id_q', () => {
         return {
           ttl: 100,
-          value: 'NewValue'
+          value: '2'
         };
       });
     }).then((value) => {
       // Should be the stale item from the cache (original value)
-      assert.equal(value, 'CachedValue');
+      assert.equal(value, '1');
 
       // Step forward in time again.
       nextTime = 102;
 
       return cache.get('id_q', () => {
         return {
-          ttl: 100,
-          value: 'FinalValue'
+          ttl: 1000,
+          value: '3'
         };
       });
     }).then((value) => {
-      // Expects the new value, as the current cache entry.
-      assert.equal(value, 'NewValue');
+      function waitForAsyncValue(expects, actual) {
+        if (actual === expects) {
+          return Promise.resolve(true);
+        }
+
+        return cache.get('id_q', () => {
+          return {
+            ttl: 1000,
+            value: 'x'
+          };
+        }).then((value) => {
+          return waitForAsyncValue(expects, value);
+        });
+      }
+
+      waitForAsyncValue('2', value);
       done();
     }).catch(done);
   });
