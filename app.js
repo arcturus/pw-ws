08ea6ea41e693c5ab6b45fcd0b49dbfdb74dda03
Francisco Jordano <fjordano@mozilla.com>
2016-06-08 16:24:50 +0100

    Single quoting all the things!

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const morgan = require('morgan');
 const app = express();
 if (config.enable_cors) {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 }
